142b46f147550a2983ac29c03a53fc7ee59b5805
Faith <fhgray96@gmail.com>
2022-06-07 21:44:30 -0500

    fetch API data

---
+++ script.js
@@ -0,0 +1,9 @@
+// http://api.openweathermap.org/data/2.5/weather?q=London&APPID=116b0c8983e86f0021af9c9c7c26ca39
+
+getData();
+
+async function getData() {
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=116b0c8983e86f0021af9c9c7c26ca39', {mode: "cors"});
+    const data = await response.json();
+    console.log(data);
+}
