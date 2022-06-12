9aa5cf36dabe6d0747cc63a17389a424bb36a995
Faith <fhgray96@gmail.com>
2022-06-11 23:19:13 -0500

    update html to prepare functions to update the DOM

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=116b0c8983e86f0021af9c9c7c26ca39
 
-getData();
+let city = "Wichita";
+let unit = "imperial";
+
+getData(city, unit);
 
 
-async function getData() {
-    let city = "Austin";
+async function getData(city, unit) {
+    // let city = "Austin";
+    // let unit = "imperial";
 
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=116b0c8983e86f0021af9c9c7c26ca39', {mode: "cors"});
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=116b0c8983e86f0021af9c9c7c26ca39&units=' + unit, {mode: "cors"});
     const data = await response.json();
     console.log(data);
+
+    document.getElementById('location').textContent = data.name;
 }
 
