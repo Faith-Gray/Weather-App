be3de2f39de8df907a547bbacc4b246f1d3e0e52
Faith <fhgray96@gmail.com>
2022-06-13 22:08:41 -0500

    add searchbar functionality

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=116b0c8983e86f0021af9c9c7c26ca39
 
-let city = "portland";
-let unit = "imperial";
+let city = "austin";
 
 // getElementById('searchBar').addEventListener('click', search);
 // function search() {
@@ -9,14 +8,31 @@ let unit = "imperial";
 //     getData(city, unit)
 // }
 
-getData(city, unit);
+function clearSearchBar() {
+    document.getElementById('searchQueryInput').value = "";
+}
 
+// getData(city);
 
-async function getData(city, unit) {
+document.getElementById('searchQuerySubmit').addEventListener('click', (e) => {
+    let value = document.getElementById('searchQueryInput').value;
+    getData(value);
+    clearSearchBar();
+})
+
+document.getElementById('searchQueryInput').addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        getData(document.getElementById('searchQueryInput').value);
+        clearSearchBar();
+
+    }
+})
+
+async function getData(city) {
     // let city = "Austin";
     // let unit = "imperial";
 
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=116b0c8983e86f0021af9c9c7c26ca39&units=' + unit, {mode: "cors"});
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=116b0c8983e86f0021af9c9c7c26ca39&units=imperial', {mode: "cors"});
     const data = await response.json();
     console.log(data);
 
@@ -24,10 +40,13 @@ async function getData(city, unit) {
     
     
     document.getElementById('location').textContent = data.name;
-    document.getElementById('feelsLike').textContent += data.main.feels_like + "°";
-    document.getElementById('windSpeed').textContent += data.wind.speed + " mph";
+    document.getElementById('feelsLike').textContent = `Feels Like - ${data.main.feels_like}°`;
+    document.getElementById('windSpeed').textContent = `Wind Speed - ${data.wind.speed} mph`;
     document.getElementById('classification').textContent = data.weather[0].description;
-    document.getElementById('humidity').textContent += data.main.humidity + "%";
+    document.getElementById('humidity').textContent = `Humidity - ${data.main.humidity}%`;
     document.getElementById('temperature').textContent = Math.round(data.main.temp) + " F°";
+    
+    document.querySelector('.leftIconGroup').classList.remove('loading');
+    document.querySelector('.rightIconGroup').classList.remove('loading');
 }
 
